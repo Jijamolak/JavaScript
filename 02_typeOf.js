b384f19ba8a877fe52c30ff03805a1c8dac41888
Jijamolak <jijaumolak23@gmail.com>
2023-12-27 11:00:48 +0530

    variables
    variables

---
+++ 02_typeOf.js
@@ -0,0 +1,30 @@
+console.log("===============Step 1=============");
+var bankName;
+
+console.log("type of bankName Before Initialization = ",typeof(bankName));
+console.log("value of bankName before initialization =",bankName);
+bankName="SBI Bank";
+console.log("type of bankName After Initialization  = ",typeof(bankName));
+console.log("value of bankName After initialization =",bankName);
+console.log("===============Step 2=============");
+var marks='90%';
+var result=typeof marks;
+console.log("Type of marks   = ", result);
+console.log("Value of marks  = ",marks);
+console.log("===============Step 3=============");
+var isWorking;
+console.log(" type of isworking Before initialization = ",typeof(isWorking));
+console.log("value of isworking before initialization =",isWorking);
+
+isWorking=true;
+console.log("type of isworking After Initialization  = ",typeof(isWorking));
+console.log("value of isworking After Initialization  = ",(isWorking));
+
+console.log("===============Step 4=============");
+var totalCount="Hundred and Seven";
+console.log("Value of totalcount is  = ",totalCount);
+console.log("datatype of totalCount is=",typeof(totalCount));
+console.log("=============update the variable totalCount===========");
+var totalCount=107;
+console.log("Value of totalcount after updating is    = ",totalCount);
+console.log("datatype of totalcount after updating is =",typeof(totalCount));
---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,8 @@
+var age=45;
+var name="Jija";
+var isMarried=true;
+console.log("Age:",age  , "," ,  "type:",typeof(age));
+console.log("Name:",name  , "," ,  "type:",typeof(name));
+console.log("Marriage Status:",isMarried  , "," ,  "type:",typeof(isMarried));
+
+console.log("============Difference between null vs undefined==========");
