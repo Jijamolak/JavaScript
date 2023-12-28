984363d065ea1af8342b22fca0e0bfbea31e671a
Jijamolak <jijaumolak23@gmail.com>
2023-12-28 10:26:36 +0530

    function assign
    function assign

---
+++ 03_funAssig01.js
@@ -0,0 +1,43 @@
+console.log("=======Step 1=======");
+function areaRectangle(){
+    var length=6;
+    var width=4;
+    area=length*width;
+    console.log("Area of Rectangle=",area);
+
+}
+areaRectangle()
+function subtraction(){
+    var n1=60;
+    var n2=40;
+    area=n1-n2;
+    console.log("Subtraction =",area);
+
+}
+subtraction()
+console.log("========Step 2=======");
+function personalDetails(firstName,lastName,collegeName){
+    console.log("My First Name is  =",firstName);
+    console.log("My Last Name is   =",lastName);
+    console.log("My  CollegeName is=",collegeName);
+
+}
+personalDetails("JIJAU","MOLAK","SANGOLA COLLEGE");
+console.log("========Step 3=======");
+function swap(n1, n2){
+    console.log("Before Swapping :::", "My First Name is-->", n1,":::" ," My Last Name is-->", n2);
+    var temp = n1;
+    n1 = n2;
+    n2 = temp;
+    console.log("After Swapping :::", "My First Name is -->",n1, ":::", "My Last Name is-->", n2);
+}
+swap("Jija", "Molak");
+console.log("========Step 4=======");
+ function addThreeValues(n1,n2,n3)
+ {
+console.log("Addition=",n1 + n2 + n3);
+
+ }
+ addThreeValues(10.23,600,40);
+ addThreeValues("Hello ","Good ","Morning");
+
