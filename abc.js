0481156cdbd20049b63377f96daa9e1dec533da7
Jijamolak <jijaumolak23@gmail.com>
2024-01-08 17:20:19 +0530

    done
    done

---
+++ abc.js
@@ -0,0 +1,9 @@
+const s2 = "I am eating apple";
+let count = 0;
+for (let index = 0; index < s2.length; index++) {
+    if (s2.charAt(index)=='a') {
+        count++;
+    }
+    console.log(count);
+    
+}
