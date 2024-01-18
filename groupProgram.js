8c9efebdc5a54ac18043c5642283ffb882d9f564
Jijamolak <jijaumolak23@gmail.com>
2024-01-18 09:34:32 +0530

    present todays
    present today

--- groupProgram.js
+++ groupProgram.js
@@ -55,3 +55,72 @@ function fact(num) {
 }
  var a=fact(5);
  console.log(a);
+console.log("========15/1======");
+const arrayNum = [ 1,2,3,4,5 ]; 
+console.log(arrayNum);
+for (let index = 0; index < arrayNum; index++) {
+  a=index*index;   
+  console.log(a);
+}
+console.log("==============");
+function n() {
+    let arr=[1,2,3,4]
+    let newArr=[];
+    for (let index = 0; index < arr.length; index++) {
+        newArr.push(arr[index]*arr[index]);
+       
+    }
+    return newArr;
+ 
+ 
+ 
+ }
+ let az=n();
+ console.log(az);
+ console.log("=============swaping of 3 variables==========");
+ function swap(arg1,arg2,arg3) {
+    var a=arg1;
+    var b=arg2;
+    var c=arg3;
+    console.log(`First number= ${a}`);
+    console.log(`second number= ${b}`);
+    console.log(`third number= ${c}`);
+    var temp;
+    temp=a;
+    b=c;
+   var d=b;
+    
+    
+    console.log(`First number= ${b}`);
+    console.log(`second number= ${d}`);
+    console.log(`third number= ${temp}`);
+    
+ }
+ swap(10,20,30)
+
+
+console.log("=======17/01/======");
+ 
+x = 0.1 ;
+y = 0.9 ;
+++x  +  x++  +  ++y  +   y++
+a=++x
+console.log(a);
+x = 0.1 ;
+y = 0.9 ;
+b=x++
+console.log(b);
+x = 0.1 ;
+y = 0.9 ;
+c=++y;
+console.log(c);
+
+x = 0.1 ;
+y = 0.9 ;
+d=y++;
+console.log(d);
+console.log(a=b+c+d);
+console.log("===========");
+x = 0.1 ;6 output;
+y = 0.9 ;
+++x  +  x++  +  ++y  +   y++
